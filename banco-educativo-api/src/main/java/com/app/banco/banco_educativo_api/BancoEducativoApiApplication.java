5f7c655edf2a5ede441b957f58a57e9313b8ddf8
Markush852 <marcus.turing01@gmail.com>
2025-11-26 19:14:13 -0300

    base : proyecto creado vacio

---
+++ banco-educativo-api/src/main/java/com/app/banco/banco_educativo_api/BancoEducativoApiApplication.java
@@ -0,0 +1,13 @@
+package com.app.banco.banco_educativo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoEducativoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoEducativoApiApplication.class, args);
+	}
+
+}
---
+++ banco-educativo-api/src/test/java/com/app/banco/banco_educativo_api/BancoEducativoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.banco.banco_educativo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoEducativoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
